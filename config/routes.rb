88e3bcfaa1e4a64a68ef858b55e3defe72b678f5
kwgch <eiichi.kendoruki@gmail.com>
2021-04-04 20:30:02 +0900

    Add api/v1 and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'points/index'
-  get 'points/show'
-  get 'points/new'
-  post "points" => "points#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
+  root to: 'home#index'
+
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :points, only: [:index, :show]
+    end
+  end
 end
