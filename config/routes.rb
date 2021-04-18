85ff6cdc16e8b06f370d51a3ee35137965254956
kwgch <eiichi.kendoruki@gmail.com>
2021-04-18 22:32:02 +0900

    Add login function by devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
-  root to: 'home#index'
+  root to: 'login#index'
+  #root to: 'home#index'
 
   namespace :api, {format: 'json'} do
     namespace :v1 do
