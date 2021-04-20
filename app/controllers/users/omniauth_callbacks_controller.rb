fbbe3b97f5524c01e8e4758e27bc3b0d6d56508b
kwgch <eiichi.kendoruki@gmail.com>
2021-04-20 23:30:32 +0900

    Add user model

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+  def google_oauth2
+    callback_from(:google_oauth2)
+  end
+
+  def callback_from(provider)
+    provider = provider.to_s
+    @user = User.find_or_create_for_oauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user
+    else
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
+end
