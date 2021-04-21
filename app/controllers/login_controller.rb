22b925ce273530bb297ed7eb25a0cf2dd9536458
kwgch <eiichi.kendoruki@gmail.com>
2021-04-21 22:11:01 +0900

    Delete login controller

--- app/controllers/login_controller.rb
+++
@@ -1,9 +0,0 @@
-class LoginController < ApplicationController
-  before_action :authenticate_user!, only: %i[index]
-  def index
-  end
-
-  def after_sign_in_path_for(resource)
-    mypage_root_path '/'
-  end
-end
