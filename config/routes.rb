29927d73b56492688871e4d5b229054dd49a70cf
kwgch <eiichi.kendoruki@gmail.com>
2021-04-21 22:11:34 +0900

    Fix root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'login/index'
+  get 'home/index'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
-  root to: 'login#index'
+  root to: 'home#index'
+  
+  #get 'home/index'
   #root to: 'home#index'
 
   namespace :api, {format: 'json'} do
